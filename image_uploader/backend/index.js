71c0b792e7c4887d68e077eed51ef8dd2342eb0e
Umar Farooq <umarfarooq567800@gmail.com>
2025-01-30 14:21:49 +0500

    cloudinary uploader

--- image_uploader/backend/index.js
+++ image_uploader/backend/index.js
@@ -1,7 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import multer from "multer";
 import cors from "cors";
 import path from "path";
+import { v2 as cloudinary } from "cloudinary";
 
 const PORT = process.env.PORT || 3001;
 
@@ -13,15 +16,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // multer storage config
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + "_" + Math.round(Math.random() * 1e9);
-    const ext = path.extname(file.originalname);
-    cb(null, file.fieldname + "_" + uniqueSuffix + ext);
-  },
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "uploads/");
+//   },
+//   filename: (req, file, cb) => {
+//     const uniqueSuffix = Date.now() + "_" + Math.round(Math.random() * 1e9);
+//     const ext = path.extname(file.originalname);
+//     cb(null, file.fieldname + "_" + uniqueSuffix + ext);
+//   },
+// });
+const storage = multer.memoryStorage();
+
+// cloudinary config
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const upload = multer({ storage: storage });
@@ -32,12 +44,82 @@ app.post("/upload", upload.single("image"), (req, res) => {
   if (!req.file) {
     return res.status(400).send("no file uploaded");
   }
-  console.log("uploaded file", req.file);
-  res.json({
-    message: "image uploaded successfully",
-    filename: req.file.filename,
-  });
+
+  const file = req.file;
+  console.log("file:", file);
+  // console.log(file.buffer); // Now file.buffer will be available
+
+  cloudinary.uploader
+    .upload_stream({ resource_type: "image" }, (error, result) => {
+      if (error) {
+        console.error("Cloudinary Error:", error);
+        return res
+          .status(500)
+          .json({ message: "Error uploading to Cloudinary" });
+      }
+      console.log("Cloudinary Result:", result);
+      res.status(200).json({
+        message: "Image uploaded successfully",
+        imageUrl: result.secure_url,
+        publicId: result.public_id,
+      });
+    })
+    .end(file.buffer);
+
+  // cloudinary.uploader
+  //   .upload(file, {
+  //     resource_type: "image",
+  //     public_id: "test-image",
+  //     overwrite: true,
+  //   })
+  //   .then((result) => {
+  //     console.log("upload result:", result);
+  //   })
+  //   .catch((error) => {
+  //     console.error("cloudinary error:", error);
+  //   });
 });
+
+// app.post("/upload", upload.single("image"), (req, res) => {
+//   if (!req.file) {
+//     return res.status(400).send("no file uploaded");
+//   }
+
+//   const file = req.file;
+//   console.log("file", file);
+//   console.log(file.buffer);
+
+//   try {
+//     cloudinary.uploader
+//       .upload_stream(
+//         (error, result) => {
+//           // Callback function here!
+//           if (error) {
+//             console.error("Cloudinary Error:", error);
+//             return res
+//               .status(500)
+//               .json({ message: "Error uploading to Cloudinary" });
+//           }
+
+//           console.log("Cloudinary Result:", result);
+
+//           res.status(200).json({
+//             message: "Image uploaded successfully",
+//             imageUrl: result.secure_url,
+//             publicId: result.public_id,
+//           });
+//         },
+//         {
+//           // Upload options here
+//           resource_type: "image",
+//           // ... other options
+//         }
+//       )
+//       .end(file.buffer);
+//   } catch (error) {
+//     console.error("cloudinary error catch", error);
+//   }
+// });
 
 app.get("/", (req, res) => {
   res.send("hey, Hello world");
